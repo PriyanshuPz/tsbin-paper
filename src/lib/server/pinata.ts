86e9533a22cd4f43e1ba04dac75cc36d8ae3805c
Priyanshu Verma <priyanshuverma@outlook.in>
2026-01-29 18:22:54 +0530

    feat: remove Pinata SDK integration from server code
    Signed-off-by: Priyanshu Verma <priyanshuverma@outlook.in>

--- src/lib/server/pinata.ts
+++
@@ -1,8 +0,0 @@
-import { PinataSDK } from "pinata";
-import { PINATA_API_JWT } from "$env/static/private";
-import { PUBLIC_GATEWAY_URL } from "$env/static/public";
-
-export const pinata = new PinataSDK({
-  pinataJwt: `${PINATA_API_JWT}`,
-  pinataGateway: `${PUBLIC_GATEWAY_URL}`,
-});
